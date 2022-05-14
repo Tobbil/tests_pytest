d2445952ae8a63e3c854808556010e615805eab3
Tobbil <kysthalden@gmail.com>
2022-05-14 23:00:39 +0200

    POM learning

--- page.py
+++ page.py
@@ -1,3 +1,4 @@
+from re import S
 import time
 from element import BasePageElement
 from locators import MainPageLocators
@@ -9,28 +10,37 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 
-class SearchTextElement(BasePageElement):
-    """This class gets the search text from the specified locator"""
 
-    #The locator for search box where search string is entered
-    locator = 'q'
-
-
-class BasePage(object):
-    """Base class to initialize the base page that will be called from all
-    pages"""
+class MainPage(object):
 
     def __init__(self, driver):
+
         self.driver = driver
+        self.MENU_PHONES = MainPageLocators.MENU_PHONES
+        self.PRICE_IN_LIST = MainPageLocators.PRICE_IN_LIST
+        self.PHONE_NAME_IN_LIST = MainPageLocators.PHONE_NAME_IN_LIST
+        self.PRICE_IN_ITEM_PAGE = MainPageLocators.PRICE_IN_ITEM_PAGE
+        self.PHONE_NAME_IN_ITEM_PAGE = MainPageLocators.PHONE_NAME_IN_ITEM_PAGE
+        self.DESCRIPTION_IN_ITEM_PAGE = MainPageLocators.DESCRIPTION_IN_ITEM_PAGE
+        self.ADD_TO_CART_BUTTON = MainPageLocators.ADD_TO_CART_BUTTON
+        self.GO_TO_CART = MainPageLocators.GO_TO_CART
+        self.CART = MainPageLocators.CART
+        self.CART_CHILDREN = MainPageLocators.CART_CHILDREN
+        self.CART_TOTAL = MainPageLocators.CART_TOTAL
+        self.TABLE = MainPageLocators.TABLE
+        self.PLACE_ORDER_BUTTON = MainPageLocators.PLACE_ORDER_BUTTON
+        self.CHECKOUT_TOTAL = MainPageLocators.CHECKOUT_TOTAL
+        self.CHECKOUT_FORM_NAME = MainPageLocators.CHECKOUT_FORM_NAME
+        self.CHECKOUT_FORM_COUNTRY = MainPageLocators.CHECKOUT_FORM_COUNTRY
+        self.CHECKOUT_FORM_CITY = MainPageLocators.CHECKOUT_FORM_CITY
+        self.CHECKOUT_FORM_CARD = MainPageLocators.CHECKOUT_FORM_CARD
+        self.CHECKOUT_FORM_MONTH = MainPageLocators.CHECKOUT_FORM_MONTH
+        self.CHECKOUT_FORM_YEAR = MainPageLocators.CHECKOUT_FORM_YEAR
+        self.CHECKOUT_SUBMIT_BUTTON = MainPageLocators.CHECKOUT_SUBMIT_BUTTON
+        self.CHECKOUT_OK_BUTTON = MainPageLocators.CHECKOUT_OK_BUTTON
 
 
-class MainPage(BasePage):
-    """Home page action methods come here. I.e. Python.org"""
-
-    #Declares a variable that will contain the retrieved text
-    search_text_element = SearchTextElement()
-
-    def handle_exceptions(self,object):
+    def handle_exceptions(self, object):
 
         try:
             element = self.driver.find_element(*object)
@@ -42,62 +52,62 @@ class MainPage(BasePage):
 
     def click_menu_phones(self):
         
-        element = self.handle_exceptions(MainPageLocators.MENU_PHONES)
+        element = self.handle_exceptions(self.MENU_PHONES)
         element.click()
 
-    def test_price_in_list(self):
+    def get_price_in_list(self):
 
-        return self.handle_exceptions(MainPageLocators.PRICE_IN_LIST)
+        return self.handle_exceptions(self.PRICE_IN_LIST)
 
 
-    def test_phone_name_in_list(self):
+    def get_phone_name_in_list(self):
 
-        return self.handle_exceptions(MainPageLocators.PHONE_NAME_IN_LIST)
+        return self.handle_exceptions(self.PHONE_NAME_IN_LIST)
  
 
-    def test_price_in_item_page(self):
+    def get_price_in_item_page(self):
 
-        return self.handle_exceptions(MainPageLocators.PRICE_IN_ITEM_PAGE)
+        return self.handle_exceptions(self.PRICE_IN_ITEM_PAGE)
  
 
-    def test_phone_name_in_item_page(self):
+    def get_phone_name_in_item_page(self):
         
-        return self.handle_exceptions(MainPageLocators.PHONE_NAME_IN_ITEM_PAGE)
+        return self.handle_exceptions(self.PHONE_NAME_IN_ITEM_PAGE)
 
-    def test_description_in_item_page(self):
+    def get_description_in_item_page(self):
 
-        return self.handle_exceptions(MainPageLocators.DESCRIPTION_IN_ITEM_PAGE)
+        return self.handle_exceptions(self.DESCRIPTION_IN_ITEM_PAGE)
 
-    def test_add_to_cart_button(self):
+    def get_add_to_cart_button(self):
 
-        return self.handle_exceptions(MainPageLocators.ADD_TO_CART_BUTTON)
+        return self.handle_exceptions(self.ADD_TO_CART_BUTTON)
 
-    def test_go_to_cart(self):
+    def get_go_to_cart(self):
 
-        return self.handle_exceptions(MainPageLocators.GO_TO_CART)
+        return self.handle_exceptions(self.GO_TO_CART)
 
     def test_cart_content(self):
 
-        parent_element = self.handle_exceptions(MainPageLocators.CART)
-        element_list = parent_element.find_elements(*MainPageLocators.CART_CHILDREN)
+        parent_element = self.handle_exceptions(self.CART)
+        element_list = parent_element.find_elements(*self.CART_CHILDREN)
         print(f"Items in cart: {len(element_list)}")
         return len(element_list) == 1
 
-    def test_cart_total(self):
+    def get_cart_total(self):
 
         timeout = time.time() + 10  
 
         while time.time() < timeout:
 
-            element = self.handle_exceptions(MainPageLocators.CART_TOTAL)
+            element = self.handle_exceptions(self.CART_TOTAL)
             if len(element.text) > 0:
                 break
 
         return element
 
-    def test_price_in_table(self):
+    def get_price_in_table(self):
 
-        element = self.handle_exceptions(MainPageLocators.TABLE)
+        element = self.handle_exceptions(self.TABLE)
         element_list = element.find_elements(By.TAG_NAME, "td")
         for i in element_list:
             text = i.text
@@ -106,55 +116,46 @@ class MainPage(BasePage):
 
         return element 
 
-    def test_phone_name_in_table(self):
+    def get_phone_name_in_table(self):
 
-        element = self.handle_exceptions(MainPageLocators.TABLE)
+        element = self.handle_exceptions(self.TABLE)
         element_list = element.find_elements(By.TAG_NAME, "td")
 
         return element_list[1]    
   
-    def test_place_order_button(self):
+    def get_place_order_button(self):
 
-        element = self.handle_exceptions(MainPageLocators.PLACE_ORDER_BUTTON)
+        element = self.handle_exceptions(self.PLACE_ORDER_BUTTON)
 
         return element
 
-    def test_total_in_checkout(self):
+    def get_total_in_checkout(self):
 
         timeout = time.time() + 10  
 
         while time.time() < timeout:
 
-            element = self.handle_exceptions(MainPageLocators.CHECKOUT_TOTAL)
+            element = self.handle_exceptions(self.CHECKOUT_TOTAL)
             if len(element.text) > 0:
                 break
 
         return element
 
-    def test_fill_out_checkout_form(self):
+    def get_checkout_form(self):
         
-        checkout_name = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_NAME)
-        checkout_country = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_COUNTRY)
-        checkout_city = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_CITY)
-        checkout_card = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_CARD)
-        checkout_month = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_MONTH)
-        checkout_year = self.handle_exceptions(MainPageLocators.CHECKOUT_FORM_YEAR)
+        checkout_name = self.handle_exceptions(self.CHECKOUT_FORM_NAME)
+        checkout_country = self.handle_exceptions(self.CHECKOUT_FORM_COUNTRY)
+        checkout_city = self.handle_exceptions(self.CHECKOUT_FORM_CITY)
+        checkout_card = self.handle_exceptions(self.CHECKOUT_FORM_CARD)
+        checkout_month = self.handle_exceptions(self.CHECKOUT_FORM_MONTH)
+        checkout_year = self.handle_exceptions(self.CHECKOUT_FORM_YEAR)
 
         return checkout_name, checkout_country, checkout_city, checkout_card, checkout_month, checkout_year
         
-    def test_submit_purchase(self):
+    def get_submit_purchase(self):
 
-        return self.handle_exceptions(MainPageLocators.CHECKOUT_SUBMIT_BUTTON)
+        return self.handle_exceptions(self.CHECKOUT_SUBMIT_BUTTON)
 
-    def test_OK_button_purchase(self):
+    def get_OK_button_purchase(self):
 
-        return self.handle_exceptions(MainPageLocators.CHECKOUT_OK_BUTTON)
-
-
-class SearchResultsPage(BasePage):
-    """Search results page action methods come here"""
-
-    def is_results_found(self):
-        # Probably should search for this text in the specific page
-        # element, but as for now it works fine
-        return "No results found." not in self.driver.page_source
+        return self.handle_exceptions(self.CHECKOUT_OK_BUTTON)
--- test_demoblaze.py
+++ test_demoblaze.py
@@ -3,6 +3,7 @@ import unittest
 import os
 import sys
 import page
+from WebDriverSetup import WebDriverSetup
 from webbrowser import Chrome
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -13,36 +14,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class TestDemoblazeAddToCart(unittest.TestCase):
+class TestDemoblazeAddToCart(WebDriverSetup):
 
     test_data = {"name":"Mr Test","country":"Norway","city":"Tromsø",
                 "card":"4376232127005830","month":"May","year":"2022"}
-
-    def setUp(self):
-        self.driver = webdriver.Chrome(os.path.join(sys.path[0], "chromedriver"))
-        self.driver.get("https://www.demoblaze.com/")
         
     def test_add_to_cart(self):
         driver = self.driver
+        driver.get("https://www.demoblaze.com/")
         test_data = self.test_data
         driver.implicitly_wait(10)
         main_page = page.MainPage(driver)
         main_page.click_menu_phones()
-        element = main_page.test_price_in_list()
+        element = main_page.get_price_in_list()
         self.assertEqual("$360",element.text)
-        element = main_page.test_phone_name_in_list()
+        element = main_page.get_phone_name_in_list()
         self.assertEqual("Samsung galaxy s6",element.text)
-        element = main_page.test_phone_name_in_list()
+        element = main_page.get_phone_name_in_list()
         element.click()
-        element = main_page.test_price_in_item_page()
+        element = main_page.get_price_in_item_page()
         price_add_to_cart_trimmed = element.text.replace(" *includes tax","")
         self.assertEqual("$360",price_add_to_cart_trimmed)
-        element = main_page.test_phone_name_in_item_page()
+        element = main_page.get_phone_name_in_item_page()
         self.assertEqual("Samsung galaxy s6",element.text)
-        element = main_page.test_description_in_item_page()
+        element = main_page.get_description_in_item_page()
         self.assertNotEqual("",element.text)
         self.assertNotEqual(" ",element.text)
-        element = main_page.test_add_to_cart_button()
+        element = main_page.get_add_to_cart_button()
         self.assertEqual("Add to cart",element.text)
         element.click()
         WebDriverWait(driver,10).until(EC.alert_is_present())
@@ -50,42 +48,39 @@ class TestDemoblazeAddToCart(unittest.TestCase):
         message = alert.text
         self.assertEqual("Product added",message)
         alert.accept()
-        element = main_page.test_go_to_cart()
+        element = main_page.get_go_to_cart()
         element.click()
         self.assertTrue(main_page.test_cart_content())
-        element = main_page.test_cart_total()
+        element = main_page.get_cart_total()
         self.assertIn(element.text,"$360")
-        element = main_page.test_price_in_table()
+        element = main_page.get_price_in_table()
         self.assertIn(element.text,"$360")
-        element = main_page.test_phone_name_in_table()
+        element = main_page.get_phone_name_in_table()
         self.assertEqual("Samsung galaxy s6",element.text)
         # tu test dla delete
 
         # tu test dla delete
-        element = main_page.test_place_order_button()
+        element = main_page.get_place_order_button()
         self.assertEqual("Place Order",element.text)
         element.click()
-        element = main_page.test_total_in_checkout()
+        element = main_page.get_total_in_checkout()
         price_checkout_trimmed = element.text.replace("Total: ","")
         self.assertEqual(price_checkout_trimmed,"360")
-        checkout_elements = main_page.test_fill_out_checkout_form()
+        checkout_elements = main_page.get_checkout_form()
         checkout_elements[0].send_keys(test_data["name"])
         checkout_elements[1].send_keys(test_data["country"])
         checkout_elements[2].send_keys(test_data["city"])
         checkout_elements[3].send_keys(test_data["card"])
         checkout_elements[4].send_keys(test_data["month"])
         checkout_elements[5].send_keys(test_data["year"])
-        time.sleep(1)
-        element = main_page.test_submit_purchase()
+        time.sleep(2)
+        element = main_page.get_submit_purchase()
         element.click()
         # tu assert danych w alercie
         
         # tu assert danych w alercie
-        element = main_page.test_OK_button_purchase()
+        element = main_page.get_OK_button_purchase()
         element.click()
-
-    def tearDown(self):
-        self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
