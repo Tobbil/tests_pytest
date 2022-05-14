3dfccdf4f8ac0d1085b5fc67094394a3cef5ba3c
Tobbil <kysthalden@gmail.com>
2022-05-14 09:00:15 +0200

    More tests and exception handling

--- locators.py
+++ locators.py
@@ -5,6 +5,17 @@ class MainPageLocators(object):
 
     MENU_PHONES = (By.LINK_TEXT, "Phones")
     PRICE_IN_LIST = (By.CSS_SELECTOR, "#tbodyid h5")
+    PRICE_IN_ITEM_PAGE = (By.CSS_SELECTOR, "#tbodyid h3")
+    PHONE_NAME_IN_LIST = (By.CSS_SELECTOR, "#tbodyid a.hrefch")
+    PHONE_NAME_IN_ITEM_PAGE = (By.CSS_SELECTOR, "#tbodyid h2")
+    DESCRIPTION_IN_ITEM_PAGE = (By.CSS_SELECTOR, "#more-information p")
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#tbodyid a.btn.btn-success.btn-lg")
+    GO_TO_CART = (By.CSS_SELECTOR, "#cartur")
+    CART = (By.CLASS_NAME, "table-responsive")
+    CART_CHILDREN = (By.ID, "tbodyid")
+    CART_TOTAL = (By.XPATH, "//*[@id='totalp']")
+    TABLE = (By.CLASS_NAME, "success") 
+    PLACE_ORDER_BUTTON = (By.CLASS_NAME, "btn.btn-success")
 
 class SearchResultsPageLocators(object):
     """A class for search results locators. All search results locators should
--- page.py
+++ page.py
@@ -1,5 +1,7 @@
 from element import BasePageElement
 from locators import MainPageLocators
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
@@ -26,45 +28,92 @@ class MainPage(BasePage):
     #Declares a variable that will contain the retrieved text
     search_text_element = SearchTextElement()
 
-    def is_title_matches(self):
-        """Verifies that the hardcoded text "Python" appears in page title"""
+    def handle_exceptions(self,object):
 
-        return "Python" in self.driver.title
+        try:
+            element = self.driver.find_element(*object)
 
-    def click_go_button(self):
-        """Triggers the search"""
-
-        element = self.driver.find_element(*MainPageLocators.GO_BUTTON)
-        element.click()
+        except (StaleElementReferenceException, NoSuchElementException):
+            element = WebDriverWait(self.driver,50).until(EC.presence_of_element_located(object))
+            
+        return element        
 
     def click_menu_phones(self):
         
-        try:
-            element = self.driver.find_element(*MainPageLocators.MENU_PHONES)
-            element.click()
+        element = self.handle_exceptions(MainPageLocators.MENU_PHONES)
+        element.click()
 
-        except StaleElementReferenceException:
-            element = WebDriverWait(self.driver,50).until(EC.presence_of_element_located(MainPageLocators.MENU_PHONES))
-            element.click()
+    def test_price_in_list(self):
 
-        except NoSuchElementException:
-            element = WebDriverWait(self.driver,50).until(EC.presence_of_element_located(MainPageLocators.MENU_PHONES))
-            element.click()
+        return self.handle_exceptions(MainPageLocators.PRICE_IN_LIST)
 
-    def go_to_price(self):
 
-        try:
-            element = self.driver.find_element(*MainPageLocators.PRICE_IN_LIST)
+    def test_phone_name_in_list(self):
 
-        except (StaleElementReferenceException, NoSuchElementException): # bez "*" przed MainPageLocators bo ma być nieunpackowany!
-            element = WebDriverWait(self.driver,50).until(EC.presence_of_element_located(MainPageLocators.PRICE_IN_LIST))
+        return self.handle_exceptions(MainPageLocators.PHONE_NAME_IN_LIST)
+ 
 
-        # except NoSuchElementException:
-        #     element = WebDriverWait(self.driver,50).until(EC.presence_of_element_located(MainPageLocators.PRICE_IN_LIST))
-            
+    def test_price_in_item_page(self):
+
+        return self.handle_exceptions(MainPageLocators.PRICE_IN_ITEM_PAGE)
+ 
+
+    def test_phone_name_in_item_page(self):
+        
+        return self.handle_exceptions(MainPageLocators.PHONE_NAME_IN_ITEM_PAGE)
+
+    def test_description_in_item_page(self):
+
+        return self.handle_exceptions(MainPageLocators.DESCRIPTION_IN_ITEM_PAGE)
+
+    def test_add_to_cart_button(self):
+
+        return self.handle_exceptions(MainPageLocators.ADD_TO_CART_BUTTON)
+
+    def test_go_to_cart(self):
+
+        return self.handle_exceptions(MainPageLocators.GO_TO_CART)
+
+    def test_cart_content(self):
+
+        parent_element = self.handle_exceptions(MainPageLocators.CART)
+        element_list = parent_element.find_elements(*MainPageLocators.CART_CHILDREN)
+        print(f"Items in cart: {len(element_list)}")
+        return len(element_list) == 1
+
+    def test_cart_total(self):
+
+        while True:
+
+            element = self.handle_exceptions(MainPageLocators.CART_TOTAL)
+            if len(element.text) > 0:
+                break
+
         return element
 
+    def test_price_in_table(self):
 
+        element = self.handle_exceptions(MainPageLocators.TABLE)
+        element_list = element.find_elements(By.TAG_NAME, "td")
+        for i in element_list:
+            text = i.text
+            if text.isnumeric():
+                element = i
+
+        return element 
+
+    def test_phone_name_in_table(self):
+
+        element = self.handle_exceptions(MainPageLocators.TABLE)
+        element_list = element.find_elements(By.TAG_NAME, "td")
+
+        return element_list[1]    
+  
+    def test_place_order_button(self):
+
+        element = self.handle_exceptions(MainPageLocators.PLACE_ORDER_BUTTON)
+
+        return element
 
 
 class SearchResultsPage(BasePage):
